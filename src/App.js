e027aa0ee8b5bd32f4593bae882472d2c51e53cc
Kanatat Tangpattanapradit <icqsang1@gmail.com>
2022-03-07 21:48:21 +0700

    add libary chatbot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ChatBot from 'react-simple-chatbot';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Review extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      gender: '',
+      age: '',
+    };
+  }
+
+  componentWillMount() {
+    const { steps } = this.props;
+    const { name, gender, age } = steps;
+
+    this.setState({ name, gender, age });
+  }
+
+  render() {
+    const { name, gender, age } = this.state;
+    return (
+      <div style={{ width: '100%' }}>
+        <h3>Summary</h3>
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{name.value}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{gender.value}</td>
+            </tr>
+            <tr>
+              <td>Age</td>
+              <td>{age.value}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+Review.propTypes = {
+  steps: PropTypes.object,
+};
+
+Review.defaultProps = {
+  steps: undefined,
+};
+
+class App extends Component {
+  render() {
+    return (
+      <ChatBot
+        steps={[{
+          id: '0',
+          message: 'สวัสดีครับ',
+          trigger: 'first',
+        },
+          {
+            id: 'first',
+            user: true,
+            trigger: (input) => {
+              console.log(input.value)
+              if (input.value.includes("ขอสั่งของ")) {
+                return 'สนใจ';
+              }else if(input.value.includes("ใส่ได้ประมาณกี่ชิ้น"))  {
+                return 'ใส่';
+              }else if(input.value.includes("ใส่ได้ประมาณกี่ชิ้น"))  {
+                return 'ใส่';
+            } 
+              else  {
+                console.log("god")
+                return 'first';
+            }
+          },
+          
+
+          },
+          {
+            id: 'ใส่',
+            message: 'ขนมของลูกค้ามีขนาดกว้าง*ยาว*สูงกี่เซนคะ',
+            trigger: 'first',
+          },
+          {
+            id: 'สนใจ',
+            message: 'ลูกค้าสนใจสินค้ารายการไหนสอบถามได้เลยนะคะ ^_^',
+            trigger: 'first',
+          },
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: 'name',
+          },
+          {
+            id: 'name',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}! What is your gender?',
+            trigger: 'gender',
+          },
+          {
+            id: 'gender',
+            options: [
+              { value: 'male', label: 'Male', trigger: '5' },
+              { value: 'female', label: 'Female', trigger: '5' },
+            ],
+          },
+          {
+            id: '5',
+            message: 'How old are you?',
+            trigger: 'age',
+          },
+          {
+            id: 'age',
+            user: true,
+            trigger: '7',
+            validator: (value) => {
+              if (isNaN(value)) {
+                return 'value must be a number';
+              } else if (value < 0) {
+                return 'value must be positive';
+              } else if (value > 120) {
+                return `${value}? Come on!`;
+              }
+
+              return true;
+            },
+          },
+          {
+            id: '7',
+            message: 'Great! Check out your summary',
+            trigger: 'review',
+          },
+          {
+            id: 'review',
+            component: <Review />,
+            asMessage: true,
+            trigger: 'update',
+          },
+          {
+            id: 'update',
+            message: 'Would you like to update some field?',
+            trigger: 'update-question',
+          },
+          {
+            id: 'update-question',
+            options: [
+              { value: 'yes', label: 'Yes', trigger: 'update-yes' },
+              { value: 'no', label: 'No', trigger: 'end-message' },
+            ],
+          },
+          {
+            id: 'update-yes',
+            message: 'What field would you like to update?',
+            trigger: 'update-fields',
+          },
+          {
+            id: 'update-fields',
+            options: [
+              { value: 'name', label: 'Name', trigger: 'update-name' },
+              { value: 'gender', label: 'Gender', trigger: 'update-gender' },
+              { value: 'age', label: 'Age', trigger: 'update-age' },
+            ],
+          },
+          {
+            id: 'update-name',
+            update: 'name',
+            trigger: '7',
+          },
+          {
+            id: 'update-gender',
+            update: 'gender',
+            trigger: '7',
+          },
+          {
+            id: 'update-age',
+            update: 'age',
+            trigger: '7',
+          },
+          {
+            id: 'end-message',
+            message: 'Thanks! Your data was submitted successfully!',
+            end: true,
+          },
+        ]}
+      />
+    );
+  }
 }
 
 export default App;
